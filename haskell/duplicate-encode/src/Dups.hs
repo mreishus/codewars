dae0b26ab4d97c44654b82192a5bafdcef4f7e84
Matthew Reishus <mreishus@users.noreply.github.com>
2019-02-20 14:56:11 -0600

    Refactor haskell/duplicate-encode

--- haskell/duplicate-encode/src/Dups.hs
+++ haskell/duplicate-encode/src/Dups.hs
@@ -12,17 +12,17 @@ duplicateEncode xs = map (encode counts) xsLower
     xsLower = map C.toLower xs
     counts = charCount xsLower
 
+-- Given a map of character frequencies and a char, encode it
 encode :: M.Map Char Int -> Char -> Char
 encode map input
-  | liftM2 (>) count (Just 1) == Just True = ')'
-  | otherwise = '('
+  | count == Nothing || count == Just 1 = '('
+  | otherwise = ')'
   where
     count = M.lookup input map
 
 -- Given a string, convert it to a map of character frequency counts
 charCount :: String -> M.Map Char Int
 charCount = foldl addCharToMap M.empty
-
--- Build up a map of character frequency counts by adding one character seen.
-addCharToMap :: M.Map Char Int -> Char -> M.Map Char Int
-addCharToMap aMap x = M.insertWith (+) x 1 aMap
+  where
+    addCharToMap :: M.Map Char Int -> Char -> M.Map Char Int
+    addCharToMap aMap x = M.insertWith (+) x 1 aMap
