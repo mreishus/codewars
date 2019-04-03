eb1b462a05ce48c95e51403471a0d3fff846a9b5
Matthew Reishus <mreishus@users.noreply.github.com>
2019-04-03 17:16:46 -0500

    Haskell/FreakContazSequence made faster algorithm

--- haskell/2k/contaz/src/FreakContazSequence.hs
+++ haskell/2k/contaz/src/FreakContazSequence.hs
@@ -3,41 +3,103 @@ module FreakContazSequence
   , nextContaz
   , stepToTake
   , numToSequence
-  , maybeFreakContazSequence
+  , bruteSeqToNum
+  , seqToNum
+  , seqToNumMulti
+  , powersOfThree
   ) where
 
 import           Data.List (find, isPrefixOf)
 
+-- Given a number, provide its list of steps for the contaz sequence.
+-- example: numToSequence 1574 = "ddDddUDUddDDD"
 numToSequence :: Integer -> String
 numToSequence n =
   fmap snd $
   takeWhile ((/= 1) . fst) $
   map (\x -> (x, stepToTake x)) $ iterate nextContaz n
 
+-- Given a number, what is the shorthand for the next step we will take in the contaz sequence?
 stepToTake :: Integer -> Char
 stepToTake n
   | n `mod` 3 == 0 = 'D'
   | n `mod` 3 == 1 = 'U'
   | n `mod` 3 == 2 = 'd'
 
+-- Given a number, find the next number in the contaz sequence.
 nextContaz :: Integer -> Integer
 nextContaz n
   | n `mod` 3 == 0 = n `quot` 3
   | n `mod` 3 == 1 = ((4 * n) + 2) `quot` 3
   | n `mod` 3 == 2 = ((2 * n) - 1) `quot` 3
 
+-- MAIN ENTRY POINT.
+-- Given a list of steps for the contaz sequence, find the smallest number whose
+-- list of steps is a prefix of the steps given.
 freakContazSequence :: String -> Int
-freakContazSequence xs = fixValue $ maybeFreakContazSequence xs
+freakContazSequence xs = fixValue $ seqToNum xs
 
+-- Turn a Just Integer into an Int.
 fixValue :: Maybe Integer -> Int
 fixValue Nothing  = 0
 fixValue (Just x) = fromIntegral x
 
-maybeFreakContazSequence :: String -> Maybe Integer
-maybeFreakContazSequence xs =
+seqToNum :: String -> Maybe Integer
+seqToNum xs
+  | l < 4 = bruteSeqToNum xs
+  | otherwise = smartSeqToNum xs
+  where
+    l = length xs
+
+smartSeqToNum :: String -> Maybe Integer
+smartSeqToNum xs
+  | prevNum == 0 = Nothing
+  | otherwise = seqToNumMulti xs (fromIntegral multi) offset
+  where
+    prevNum = fixValue $ seqToNum $ init xs
+    multi = largestPow3Under prevNum
+    offset = fromIntegral $ prevNum `mod` multi
+
+powersOfThree :: Num a => [a]
+powersOfThree = [3 ^ i | i <- [1 ..]]
+
+largestPow3Under :: (Ord a, Num a) => a -> a
+largestPow3Under x = last $ takeWhile (< x) powersOfThree
+
+-- Given a list of steps for the contaz sequence, find the smallest number whose
+-- list of steps is a prefix of the steps given.
+-- Brute force, quite slow for larger numbers.
+bruteSeqToNum :: String -> Maybe Integer
+bruteSeqToNum xs = find (\n -> xs `isPrefixOf` (numToSequence n)) range
+  where
+    range = [1 .. 10 ^ 10]
+
+-- Like bruteSeqToNum, but only searches some numbers.
+-- If you provide multi = 9 and offset = 3,
+-- it will only search numbers where (num `mod` 9) == 3
+seqToNumMulti :: String -> Integer -> Integer -> Maybe Integer
+seqToNumMulti xs multi offset =
   find (\n -> xs `isPrefixOf` (numToSequence n)) range
   where
-    range = [1 .. 10 ^ 8]
+    range = [(i * multi) + offset | i <- [1 .. 10 ^ 7]]
+{-
+*Main FreakContazSequence Lib> seqToNumMulti "ddDddUDUDUDUddDUUUdd" 2187 1574
+Just 1883021696
+
+That worked reasonably quickly.  The key concept is
+bruteSeqToNum "ddDddUDUDU" = 8135.   8135 `mod` 2187 = 1574
+3^7 = 2187..
+
+We can do 3^8, too = 6561.  So check 8135 `mod` 6561 = 1574
+Now run
+seqToNumMulti "ddDddUDUDUDUddDUUUdd" 6561 1574
+-}
+{-
+getRange (x:xs)
+  | x == 'D' = [i | i <- [1 .. 10 ^ 10], i `mod` 3 == 0]
+  | x == 'd' = [i | i <- [1 .. 10 ^ 10], i `mod` 3 == 2]
+  | x == 'U' = [i | i <- [1 .. 10 ^ 10], i `mod` 3 == 1]
+-}
 {-
     Freak Contaz Sequence is another version of Collatz sequence of integers is
     obtained from the starting a(1) in the following way: ``` a(n + 1) = a(n) / 3
@@ -76,4 +138,99 @@ maybeFreakContazSequence xs =
     [output] integer N
 
     The answer N fits in a integer, 2 <= N <= 10^10.
+-}
+{-
+D
+DD
+DU
+Dd
+
+U  4
+UD 4
+UU
+Ud
+
+d  5
+dD 5
+dU
+dd
+
+"D" - Divisible by 3 = 0
+"DD" - Divisble by 9 = 0
+(9,"DD"),
+(18,"DDd"),
+(27,"DDD"),
+(36,"DDUDd"),
+(45,"DDdD"),
+(54,"DDDd"),
+
+"D" - Divisible by 3 = 0
+"DU" - mod by 9 = 3
+(12,"DUDd"),
+(21,"DUUdDD"),
+(30,"DUdDD"),
+(39,"DUDDd"),
+(48,"DUUDUdDD"),
+(57,"DUdddUUdDD"),
+
+"D" - Divisible by 3 = 0
+"Dd" - mod by 9 = 6
+(6,"Dd"),
+(15,"DdD"),
+(24,"DddD"),
+(33,"DdUUdDD"),
+(42,"DdDD"),
+(51,"DddUUdDD"),
+(60,"DdUDDd")]
+
+"U" - Mod by 3 = 1
+"Ud" - mod by 9 = 1
+(10,"UdDD"),
+(19,"UdddUUdDD"),
+(28,"UdUUUddDDD"),
+(37,"UdDdUUdDD"),
+(46,"UddDDD"),
+(55,"UdUDUDUdDD"),
+
+"U" - Mod by 3 = 1
+"UD" - mod by 9 = 4
+(13,"UDDd"),
+(22,"UDUdDD"),
+(31,"UDdDD"),
+(40,"UDDDd"),
+(49,"UDUDUdDD"),
+(58,"UDdddUUdDD"),
+
+"U" - Mod by 3 = 1
+"UU" - mod by 9 = 7
+(7,"UUdDD"),
+(16,"UUDUdDD"),
+(25,"UUUddDDD"),
+(34,"UUddDDD"),
+(43,"UUDdddUUdDD"),
+(52,"UUUDDdDD"),
+
+"dD" - mod by 9 = 5
+(14,"dDD"),
+(23,"dDdD"),
+(32,"dDUUdDD"),
+(41,"dDDD"),
+(50,"dDdUUdDD"),
+(59,"dDUDDd"),
+
+"dU" - mod by 9 = 2
+(11,"dUUdDD"),
+(20,"dUDDd"),
+(29,"dUdddUUdDD"),
+(38,"dUUUddDDD"),
+(47,"dUDdDD"),
+(56,"dUdDdUUdDD"),
+
+"dd" - mod by 9 = 8
+(8,"ddD"),
+(17,"ddUUdDD"),
+(26,"dddUUdDD"),
+(35,"ddDdD"),
+(44,"ddUdddUUdDD"),
+(53,"dddDdD"),
 -}
